3c1fb386d9379c638e50c94eca0d69340c2e17db
davimut <davimuto@gmail.com>
2024-04-23 21:43:30 +0200

    add : correzione creazione costruttore Rettangolo richimato in scanne

--- src/org/lessons/java/geometria/Main.java
+++ src/org/lessons/java/geometria/Main.java
@@ -1,23 +1,23 @@
 package org.lessons.java.geometria;
+import java.util.Scanner;
 
 
 public class Main {
-            
-	public static void main(String[] args) {
-		
-	            
-	        Rettangolo rettangolo = new Rettangolo();
-	        
-	    
-	        System.out.println("Perimetro = " + rettangolo.calcolaPerimetro());
-	        
-	       
-	        System.out.println("Area = " + rettangolo.calcolaArea());
-	        
-	
-	        rettangolo.disegnaRettangolo();
-	};	
-	
-      
-}
 
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("inserisci la base del rettangolo:");
+		int base = scanner.nextInt();
+		System.out.println("inserisci l'altezza del rettangolo");
+		int altezza = scanner.nextInt();
+
+		Rettangolo rettangolo = new Rettangolo(base, altezza);
+
+		System.out.println("Perimetro = " + rettangolo.calcolaPerimetro());
+
+	    System.out.println("Area = " + rettangolo.calcolaArea());
+
+	    rettangolo.disegna();
+	}
+}
--- src/org/lessons/java/geometria/Rettangolo.java
+++ src/org/lessons/java/geometria/Rettangolo.java
@@ -1,32 +1,26 @@
 package org.lessons.java.geometria;
 
-import java.util.Scanner;
-
-
-
 public class Rettangolo {
-     int base;
-     int altezza;
-    Scanner scanner = new Scanner(System.in);
 
-    Rettangolo() {
-        System.out.println("Inserisci la base del rettangolo:");
-        base = scanner.nextInt();
-        System.out.println("Inserisci l'altezza del rettangolo:");
-        altezza = scanner.nextInt();
-    }
+	int base;
+	int altezza;
+
+    Rettangolo(int base, int altezza){
+	this.base = base ;
+	this.altezza = altezza;
+    };
 
     int calcolaPerimetro() {
-        int Perimetro = (altezza + base) * 2;
-        return Perimetro;
-    }
+    int Perimetro = (altezza + base) * 2 ;
+	return Perimetro;
+    };
 
     int calcolaArea() {
-        int Area = base * altezza;
-        return Area;
-    }
+    int Area =  base * altezza ;
+	return Area;
+    };
 
-    public void disegnaRettangolo() {
+    public void disegna() {
         for (int i = 0; i < altezza; i++) {
             for (int n = 0; n < base; n++) {
                 if (i == 0 || i == altezza - 1 || n == 0 || n == base - 1) {
@@ -38,4 +32,7 @@ public class Rettangolo {
             System.out.println();
         }
     }
+
+
+
 }
